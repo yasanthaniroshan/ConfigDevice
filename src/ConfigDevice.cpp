eda9e452f62fc8cfcad0be0b196741ae06ea5ddd
Yasantha Niroshan <niroshanyi@gmail.com>
2025-10-03 17:10:18 +0530

    Added Minor Update - Removed the Definition

--- src/ConfigDevice.cpp
+++ src/ConfigDevice.cpp
@@ -4,9 +4,5 @@ void configDevice() {
   uint64_t chipid = ESP.getEfuseMac(); 
   char idStr[17];
   sprintf(idStr, "%016llX", chipid); 
-  #if defined(DEVICE_ID)
-    while (!String(idStr).equals(DEVICE_ID)) delay(1000);
-  #else
-    while(1) delay(1000);
-  #endif
+  while (!String(idStr).equals(MQTT_CLIENT_ID)) delay(1000);
 }
--- src/ConfigDevice.h
+++ src/ConfigDevice.h
@@ -2,6 +2,7 @@
 #define CONFIG_DEVICE_H
 #include <Arduino.h>
 
+extern const char* MQTT_CLIENT_ID;
 void configDevice();
 
 #endif // CONFIG_DEVICE_H
